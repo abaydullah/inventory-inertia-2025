77eeeb0e82039b48eae506ee15b4f4aa872bedeb
MD Mim Abaydullah <mimobaydullah@gmail.com>
2025-01-31 06:41:23 +0600

    code cleanup

--- routes/web.php
+++ routes/web.php
@@ -26,47 +26,9 @@ use Inertia\Inertia;
 
 
 Route::get('/clear-cache', function () {
-
     Artisan::call('cache:clear');
-    session()->put('sujon', 'cbcbcb');
 });
-//Route::domain('{tenant:sub_domain}.' . config('app.url'))->name('tenant.')->group(function () {
-//
-//    Route::get('/admin/dashboard', BackendDashboardController::class)->name('admin.dashboard');
-//
-//    Route::middleware('auth')->prefix('admin')->group(function () {
-//        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//
-//        Route::resource('customers', CustomerController::class);
-//        Route::resource('groups', GroupController::class);
-//        Route::resource('suppliers', SupplierController::class);
-//        Route::resource('products', ProductController::class);
-//        Route::resource('categories', CategoryController::class);
-//        Route::resource('accounts', AccountController::class);
-//        Route::resource('purchases', PurchaseController::class);
-//        Route::resource('invoices', InvoiceController::class);
-//
-//
-//        Route::resource('subjects', SubjectController::class);
-//        Route::resource('tests', TestController::class);
-//        Route::resource('marks', MarkController::class);
-//        Route::resource('students', StudentController::class);
-//        Route::resource('instructors', InstructorController::class);
-//        Route::resource('banners', BannerController::class);
-//        Route::resource('courses', CourseController::class);
-//        Route::resource('notices', NoticeController::class);
-//    });
-//    Route::post('/language', LanguageController::class)->name('language.store');
-//});
-//Route::domain('{subdomain}.' . config('app.url'))->group(function () {
-//    Route::get('/admin/dashboard', function (string $subdomain) {
-//
-//        $users = \App\Models\User::all();
-//        return Inertia::render('Dashboard', ['users' => $users]);
-//    })->name('admin.dashboard');
-//});
+
 Route::domain(config('app.url'))->group(function () {
     Route::get('admin/dashboard', function () {
 
